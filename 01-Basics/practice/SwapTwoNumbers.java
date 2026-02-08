268ac219e15224406751e2bdc6ce3ca74c21dfc9
SachinSharma22 <s830sharma@gmail.com>
2026-02-08 21:35:51 +0530

    swap two numbers program practice purpose

---
+++ 01-Basics/practice/SwapTwoNumbers.java
@@ -0,0 +1,17 @@
+package practice;
+
+public class SwapTwoNumbers {
+    public static void main(String[] args) {
+        int a = 10, b = 20, temp = 0;
+        System.out.println("------------ Before Swap ------------------");
+        System.out.println("a = " + a + " b = " + b);
+
+        temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println("------------ After Swap ------------------");
+        System.out.println("a = " + a + " b = " + b);
+
+    }
+}
