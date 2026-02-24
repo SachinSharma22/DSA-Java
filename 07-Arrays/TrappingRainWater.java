477172f0c31399ba3ba773a970bf3b0ba83dfd07
SachinSharma22 <s830sharma@gmail.com>
2026-02-24 12:30:50 +0530

    added another function with same time complexity to perform trapping rain water

--- 07-Arrays/TrappingRainWater.java
+++ 07-Arrays/TrappingRainWater.java
@@ -1,4 +1,27 @@
 public class TrappingRainWater {
+    public static int trappedRainWater2(int[] height){
+        if(height == null || height.length < 3){
+            return 0;
+        }
+        int n = height.length;
+        int leftMax[] = new int[n];
+        leftMax[0] = height[0];
+        int rightMax[] = new int[n];
+        int k = n;
+        rightMax[k-1] = height[k-1];
+        int m = 2;
+        for (int i = 1; i < n; i++) {
+            leftMax[i] = Math.max(height[i], leftMax[i-1]);
+            rightMax[k-m] = Math.max(height[k-m], rightMax[k-m+1]);
+            m++;
+        }
+        int trappedWater = 0;
+        for (int i = 0; i < n; i++) {
+            int waterLevel = Math.min(leftMax[i], rightMax[i]);
+            trappedWater += waterLevel - height[i];
+        }
+        return trappedWater;
+    }
     public static int trappedRainWater(int[] height){
         if (height == null || height.length < 3) {
             return 0;
@@ -27,7 +50,7 @@ public class TrappingRainWater {
     }
     public static void main(String[] args) {
         int arr[] = {4,2,0,6,3,2,5};
-        int trappedWater = trappedRainWater(arr);
+        int trappedWater = trappedRainWater2(arr);
         System.out.println("Total water trapped is: " + trappedWater);
     }
 }
