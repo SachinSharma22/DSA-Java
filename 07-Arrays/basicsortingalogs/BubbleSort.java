281d21184358ee08e95fb8b90c14faa675a28d58
SachinSharma22 <s830sharma@gmail.com>
2026-02-24 22:00:27 +0530

    Sort an unsorted array with the help of bubble sort algorithm

---
+++ 07-Arrays/basicsortingalogs/BubbleSort.java
@@ -0,0 +1,23 @@
+package basicsortingalogs;
+
+public class BubbleSort {
+    public static int[] bubbleSort(int [] arr){
+        for (int i = 0; i < arr.length-1; i++) {
+            for (int j = 0; j < arr.length-1-i; j++) {
+                if(arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        return arr;
+    }
+    public static void main(String[] args) {
+        int arr[] = {5,4,1,2,3};
+        int[] sortedArray = bubbleSort(arr);
+        for (int i = 0; i < sortedArray.length; i++) {
+            System.out.print(sortedArray[i] + " ");
+        }
+    }
+}
