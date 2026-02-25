f15d7c948fea1656e4600f1469008d929a630de2
SachinSharma22 <s830sharma@gmail.com>
2026-02-25 11:37:03 +0530

    sort array using Java inbuilt function

---
+++ 07-Arrays/basicsortingalogs/InbuiltSort.java
@@ -0,0 +1,13 @@
+package basicsortingalogs;
+
+import java.util.Arrays;
+
+public class InbuiltSort {
+    public static void main(String[] args) {
+        int arr[] = {5,4,1,2,3};
+        Arrays.sort(arr);
+       for (int i = 0; i < arr.length; i++) {
+        System.out.print(arr[i] + " ");
+       }
+    }
+}
