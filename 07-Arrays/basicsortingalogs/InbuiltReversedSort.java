92778cf0d109204bc29f5a19c0cb22588879e638
SachinSharma22 <s830sharma@gmail.com>
2026-02-25 11:50:08 +0530

    Sort array in reversed order using Collections.reversedOrder() java inbuilt function

---
+++ 07-Arrays/basicsortingalogs/InbuiltReversedSort.java
@@ -0,0 +1,14 @@
+package basicsortingalogs;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class InbuiltReversedSort {
+    public static void main(String[] args) {
+        Integer arr[] = {5,4,1,2,3};
+        Arrays.sort(arr,Collections.reverseOrder());
+        for (int i = 0; i < arr.length; i++) {
+        System.out.print(arr[i] + " ");
+       }
+    }
+}
