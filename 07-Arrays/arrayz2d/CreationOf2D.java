718a09af74caf972720ab2e6c68046fb8a37b7ac
SachinSharma22 <s830sharma@gmail.com>
2026-02-26 21:57:05 +0530

    method added to search any element is available in  given matrix or not

--- 07-Arrays/arrayz2d/CreationOf2D.java
+++ 07-Arrays/arrayz2d/CreationOf2D.java
@@ -3,6 +3,19 @@ package arrayz2d;
 import java.util.Scanner;
 
 public class CreationOf2D {
+    public static void searchElement(int[][] matrix, int key){
+        
+         for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if(matrix[i][j] == key){
+                    System.out.println("Value is available on index [ " + i +" " + j + " ]");
+                    return;
+                }
+            }
+        }
+        System.out.println("Key not found.");  
+        return;      
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int matrix[][] = new int[3][3];
@@ -18,6 +31,7 @@ public class CreationOf2D {
             }
             System.out.println();
         }
+        searchElement(matrix, 55);
         sc.close();
     }
 }
