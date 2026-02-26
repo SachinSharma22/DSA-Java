76a9ff40e8786dfd59282b30682bb30323c1cccf
SachinSharma22 <s830sharma@gmail.com>
2026-02-26 14:28:25 +0530

    reverse array method is written for rotate array k time without using extra space

--- 07-Arrays/RotateKTimes.java
+++ 07-Arrays/RotateKTimes.java
@@ -1,4 +1,17 @@
 public class RotateKTimes {
+    public static int[] reverseArray(int[] arr){
+        int n = arr.length;
+        int start = 0;
+        int end = n-1;
+       while(start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+        return arr;
+    }
      public static int[] rotateArray(int[] arr, int k){
         int n = arr.length;
         k = k%n;
@@ -14,8 +27,9 @@ public class RotateKTimes {
     }
     public static void main(String[] args) {
         int arr [] = {1,2,3,4,5};
-        int rotationTime = 10002;
-        int result[] = rotateArray(arr,rotationTime);
+        int rotationTime = 3;
+        // int result[] = rotateArray(arr,rotationTime);
+        int result[] = reverseArray(arr);
         for (int i = 0; i < result.length; i++) {
             System.out.print(result[i] + " ");
         }
