eff0bad9586934f122b73db592db452a1bf61b76
SachinSharma22 <s830sharma@gmail.com>
2026-03-05 09:37:54 +0530

    get ith position of a binary number

---
+++ 10-BitManipulation/GetIthBit.java
@@ -0,0 +1,13 @@
+public class GetIthBit {
+    public static int getIthBit(int num, int i){
+        int bitMask = 1<<i;
+        if((num & bitMask) == 0){
+            return 0;
+        }else{
+            return 1;
+        }
+    }
+    public static void main(String[] args) {
+        System.out.println(getIthBit(10,2));
+    }
+}
