15f4348735af282bc6fc9d5729f6c9351a3054bb
SachinSharma22 <s830sharma@gmail.com>
2026-03-05 09:01:52 +0530

    One's complement  uses in java

---
+++ 10-BitManipulation/OnesComplement.java
@@ -0,0 +1,9 @@
+public class OnesComplement {
+    public static void main(String[] args) {
+        /*~ This symbol generally change the 1 bit into 0 and 0 into 1 */
+        System.out.println("~ This symbol in java is known as 1's complement.");
+        // System.out.println(~1);
+        // System.out.println(~0);
+        System.out.println(~5);
+    }
+}
