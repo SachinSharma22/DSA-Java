70470cff63e8dd1045303b7dda1516ba2423c9a1
SachinSharma22 <s830sharma@gmail.com>
2026-03-05 13:14:44 +0530

    learn to clear last i bits

---
+++ 10-BitManipulation/ClearLastIBit.java
@@ -0,0 +1,9 @@
+public class ClearLastIBit {
+    public static int clearLastIBit(int n, int i){
+        int bitMask = (~0)<< i;
+        return n & bitMask;
+    }
+    public static void main(String[] args) {
+        System.out.println(clearLastIBit(15, 2));
+    }
+}
