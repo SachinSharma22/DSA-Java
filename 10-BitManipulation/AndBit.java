23b065b433297c9ae712e4b30ca9871c8e91850d
SachinSharma22 <s830sharma@gmail.com>
2026-03-05 08:41:10 +0530

    Bitwise operators like AND OR and XOR basics

---
+++ 10-BitManipulation/AndBit.java
@@ -0,0 +1,15 @@
+public class AndBit {
+    public static void main(String[] args) {
+        System.out.println("---------------------AND Bit -----------------------------");
+        System.out.println( 5 & 6);
+
+        // OR Bit 
+        System.out.println("---------------------OR Bit -----------------------------");
+        System.out.println(5 | 6);
+
+        // XOR BIT
+
+        System.out.println("---------------------XOR Bit -----------------------------");
+        System.out.println(5^6);
+    }
+}
