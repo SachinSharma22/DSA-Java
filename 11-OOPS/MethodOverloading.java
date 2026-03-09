d7ba5739c2e04093a83ca4c57a5d1590db600fc9
SachinSharma22 <s830sharma@gmail.com>
2026-03-09 15:00:06 +0530

    change calculator class name to calculator12 because it occures some error having several classess with same name in a folder

--- 11-OOPS/MethodOverloading.java
+++ 11-OOPS/MethodOverloading.java
@@ -1,13 +1,13 @@
 public class MethodOverloading {
     public static void main(String[] args) {
-        Calculator calc = new Calculator();
+        Calculator12 calc = new Calculator12();
         System.out.println(calc.sum(1, 2));
         System.out.println(calc.sum((float)1.2, (float)1.3));
         System.out.println(calc.sum(1, 2,3));
     }
 }
 
-class Calculator{
+class Calculator12{
     int sum(int a, int b){
         return a+b;
     }
