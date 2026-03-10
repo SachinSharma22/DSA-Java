71f0ddf024235b261d61aa5f4f87d2e7985c1854
SachinSharma22 <s830sharma@gmail.com>
2026-03-10 23:13:36 +0530

    optimized power calculation function added

--- 12-Recursion/ExpoVal.java
+++ 12-Recursion/ExpoVal.java
@@ -1,4 +1,16 @@
 public class ExpoVal {
+    public static int optimizedPower(int n, int x){
+        if(x == 0){
+            return 1;
+        }
+        int halfPowerSq = optimizedPower(n, x/2) *optimizedPower(n, x/2);
+        
+        if(x % 2 != 0){
+            halfPowerSq = n * halfPowerSq;
+        }
+
+        return halfPowerSq;
+    }
     public static int expoVal(int n, int x){
         if(n == 0){
             return 0;
@@ -14,7 +26,8 @@ public class ExpoVal {
     public static void main(String[] args) {
         int n = 2;
         int x = 10;
-        int result = expoVal(n, x);
+        // int result = expoVal(n, x);
+        int result = optimizedPower(n, x);
         System.out.println(result);
     }
 }
