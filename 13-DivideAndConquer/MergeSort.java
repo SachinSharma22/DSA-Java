e78c55103f7a54655f7e414537802f162412d651
SachinSharma22 <s830sharma@gmail.com>
2026-03-13 22:41:39 +0530

    merge sort code

---
+++ 13-DivideAndConquer/MergeSort.java
@@ -0,0 +1,48 @@
+public class MergeSort {
+    public static void mergeSort(int[] arr, int si, int ei){
+        if(si >= ei){
+            return;
+        }
+        int mid = si+(ei-si)/2;
+        mergeSort(arr, si, mid);
+        mergeSort(arr, mid+1, ei);
+        merge(arr, si, mid, ei);
+    }
+    public static void merge(int[] arr, int si, int mid, int ei){
+    int temp[] = new int[ei-si+1];
+
+    int i = si;       // left part
+    int j = mid+1;    // right part
+    int k = 0;
+
+    while(i <= mid && j <= ei){
+        if(arr[i] < arr[j]){
+            temp[k++] = arr[i++];
+        }else{
+            temp[k++] = arr[j++];
+        }
+    }
+
+    // remaining left
+    while(i <= mid){
+        temp[k++] = arr[i++];
+    }
+
+    // remaining right
+    while(j <= ei){
+        temp[k++] = arr[j++];
+    }
+
+    // copy to original array
+    for(k = 0, i = si; k < temp.length; k++, i++){
+        arr[i] = temp[k];
+    }
+}
+    public static void main(String[] args) {
+        int [] arr = {6,3,9,5,2,8};
+        mergeSort(arr, 0, arr.length-1);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
