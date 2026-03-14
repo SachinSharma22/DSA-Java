4affd73aae95d11f4b346940ba66c033657dcdda
SachinSharma22 <s830sharma@gmail.com>
2026-03-14 16:43:43 +0530

    reverse a string using StringBuilder

--- 12-Recursion/RecRevStr.java
+++ 12-Recursion/RecRevStr.java
@@ -1,4 +1,11 @@
 public class RecRevStr {
+    public static void revStrWithBuilder(String str, int i, StringBuilder res){
+        if(i == str.length()){
+            return;
+        }
+        revStrWithBuilder(str, i+1, res);
+        res.append(str.charAt(i));
+    }
     public static void revStr(String str, int i){
         if(i == str.length()-1){
             System.out.print(str.charAt(i) + " ");
@@ -10,6 +17,11 @@ public class RecRevStr {
     public static void main(String[] args) {
         String str = "hello";
         int i = 0;
-        revStr(str,i);
+        // revStr(str,i);
+
+        //for string builder
+        StringBuilder res = new StringBuilder();
+        revStrWithBuilder(str,i,res);
+        System.out.println(res.toString());
     }
 }
