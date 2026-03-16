2b848325e5d1143e2a55f2962fd4d7b16989f93c
SachinSharma22 <s830sharma@gmail.com>
2026-03-16 23:06:42 +0530

    different operation performed like get, set, delete and contains for a given list

--- 15-ArrayList/OperationsInArrayList.java
+++ 15-ArrayList/OperationsInArrayList.java
@@ -9,5 +9,24 @@ public class OperationsInArrayList {
         list.add(5);
         list.add(6);
         System.out.println(list);
+
+        // System.out.println("---------------Get Operation -------------------");
+
+        // // Get element
+        // int ele = list.get(2);
+        // System.out.println("Element is : " + ele);
+
+        // System.out.println("---------------Delete Operation -------------------");
+        // list.remove(3);
+        // System.out.println(list);
+
+        // System.out.println("---------------Set Operation -------------------");
+        // list.set(2, 22);
+        // System.out.println(list);
+
+        System.out.println("---------------Contains Operation -------------------");
+        System.out.println(list.contains(3));
+        System.out.println(list.contains(33));
+
     }
 }
