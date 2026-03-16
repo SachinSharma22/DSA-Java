bd767820d35d1cefd494ee33ad352dc6a0d6e7ba
SachinSharma22 <s830sharma@gmail.com>
2026-03-16 22:55:47 +0530

    initial commit to add element in arraylist

---
+++ 15-ArrayList/OperationsInArrayList.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class OperationsInArrayList {
+    public static void main(String[] args) {
+        ArrayList<Integer> list= new ArrayList<>();
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+        System.out.println(list);
+    }
+}
