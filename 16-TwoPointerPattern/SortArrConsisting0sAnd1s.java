7911a76cec268b87231659a9a635bf4fbe654123
SachinSharma22 <s830sharma@gmail.com>
2026-03-18 01:03:26 +0530

    sort array consisting only 0s and 1s with the help of two pointer method

---
+++ 16-TwoPointerPattern/SortArrConsisting0sAnd1s.java
@@ -0,0 +1,31 @@
+public class SortArrConsisting0sAnd1s {
+    public static void sortArr(int[] arr){
+        int lp = 0;
+        int rp = arr.length-1;
+        while (lp != rp) {
+            if(arr[rp] == 0 && arr[lp] == 1){
+                int temp = arr[rp];
+                arr[rp] = arr[lp];
+                arr[lp] = temp;
+            }
+
+            if(arr[rp] == 1){
+                rp--;
+            }
+            if(arr[lp] == 0){
+                lp++;
+            }
+        }
+        printArr(arr);
+    }
+    public static void printArr( int[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int[] arr = {1,0,0,1,0,1,1,0,0};
+        sortArr(arr);
+    }
+}
