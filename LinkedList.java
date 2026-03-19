13914fb98596af7026e4f7171bfc1c8151b6d601
SachinSharma22 <s830sharma@gmail.com>
2026-03-19 18:06:38 +0530

    code to addFirst, addLast and add in the middle of the Linked List

--- LinkedList.java
+++ LinkedList.java
@@ -27,11 +27,52 @@ public class LinkedList {
         head = newNode;
 
     }
+
+    public void addLast(int data){
+        Node newNode = new Node(data);
+        if(head == null){
+            head = tail = newNode;
+            return;
+        }
+        tail.next = newNode;
+        tail = newNode;
+    }
+
+    public void add(int idx, int data){
+        Node newNode = new Node(data);
+        int i = 0;
+        Node temp = head;
+
+        while(i < idx-1){
+            temp = temp.next;
+            i++;
+        }
+        newNode.next = temp.next;
+        temp.next = newNode;
+    }
+
+    public void printLList(){
+        if(head == null){
+            System.out.println("LinkedList is empty.");
+        }
+        Node temp = head;
+        while(temp != null){
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
     public static void main(String[] args) {
         LinkedList ll = new LinkedList();
-        ll.head = new Node(1);
-        ll.head.next = new Node(2);
-        ll.addFirst(1);
+        // ll.printLList();
         ll.addFirst(2);
+        // ll.printLList();
+        ll.addFirst(1);
+        // ll.printLList();
+        ll.addLast(3);
+        // ll.printLList();
+        ll.addLast(4);
+        ll.add(2, 9);
+        ll.printLList();
     }
 }
