b4691ec920a0feeb933a15ee96691e02303c2fd2
SachinSharma22 <s830sharma@gmail.com>
2026-03-25 18:02:11 +0530

    find next smaller element in the right side of the given array

--- 18-Stack/NextGreaterEleL.java
+++ 18-Stack/NextGreaterEleL.java
@@ -1,6 +1,25 @@
 import java.util.Stack;
 
 public class NextGreaterEleL {
+    public static void nextSmallerRight(int[] arr) {
+        Stack<Integer> s = new Stack<>();
+        int[] nextGrL = new int[arr.length];
+        
+         for(int i =arr.length -1; i >= 0; i--) {
+            while (!s.isEmpty() && arr[s.peek()] > arr[i]) {
+                s.pop();
+            }
+            if(s.isEmpty()){
+                nextGrL[i] = -1;
+            }else{
+                nextGrL[i] = arr[s.peek()];
+            }
+            s.push(i);
+        }
+        for (int i = 0; i < nextGrL.length; i++) {
+            System.out.print(nextGrL[i] + " ");
+        }
+    }
     public static void nextSmall(int[] arr) {
         Stack<Integer> s = new Stack<>();
         int[] nextGrL = new int[arr.length];
@@ -43,6 +62,10 @@ public class NextGreaterEleL {
         int[] arr = {1,3,2,4};
         // nextGreaterL(arr);
 
-        nextSmall(arr);
+        // This function is to find next smaller element in the left side of the array
+        // nextSmall(arr);
+
+        // This function is to find next smaller element in the right side of the array
+        nextSmallerRight(arr);
     }
 }
