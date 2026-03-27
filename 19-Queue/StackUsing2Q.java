65396d3b39ed45880eb0e143a7f55ce4a30acba5
SachinSharma22 <s830sharma@gmail.com>
2026-03-27 22:16:20 +0530

    implementing stack using 2 queue

---
+++ 19-Queue/StackUsing2Q.java
@@ -0,0 +1,84 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class StackUsing2Q {
+    static class StackQ {
+        static Queue<Integer> q1 = new LinkedList<>();
+        static Queue<Integer> q2 = new LinkedList<>();
+
+        public boolean isEmpty() {
+            return q1.isEmpty() && q2.isEmpty();
+        }
+
+        // push
+        public void push(int data) {
+            if (!q1.isEmpty()) {
+                q1.add(data);
+            } else {
+                q2.add(data);
+            }
+        }
+
+        // pop
+        public int pop() {
+            if (isEmpty()) {
+                System.out.println("Stack is Empty");
+                return -1;
+            }
+
+            int top = -1;
+
+            if (!q1.isEmpty()) {
+                while (!q1.isEmpty()) {
+                    top = q1.remove();
+                    if (q1.isEmpty()) break;
+                    q2.add(top);
+                }
+            } else {
+                while (!q2.isEmpty()) {
+                    top = q2.remove();
+                    if (q2.isEmpty()) break;
+                    q1.add(top);
+                }
+            }
+
+            return top;
+        }
+
+        // peek
+        public int peek() {
+            if (isEmpty()) {
+                System.out.println("Stack is Empty");
+                return -1;
+            }
+
+            int top = -1;
+
+            if (!q1.isEmpty()) {
+                while (!q1.isEmpty()) {
+                    top = q1.remove();
+                    q2.add(top);
+                }
+            } else {
+                while (!q2.isEmpty()) {
+                    top = q2.remove();
+                    q1.add(top);
+                }
+            }
+
+            return top;
+        }
+    }
+
+    public static void main(String[] args) {
+        StackQ s = new StackQ();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+
+        while (!s.isEmpty()) {
+            System.out.print(s.peek() + " ");
+            s.pop();
+        }
+    }
+}
