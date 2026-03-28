845fbffb8b2989ee6ba63a9475f1b64b87df25d0
SachinSharma22 <s830sharma@gmail.com>
2026-03-28 09:31:38 +0530

    Find first non-reapeting character from the string with the help of queue

---
+++ 19-Queue/FirstNonRepeatingLetter.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.util.LinkedList;
+
+public class FirstNonRepeatingLetter {
+    public static void printFirstNonRepeating(String str) {
+       Queue<Character> q = new LinkedList<>();
+       int freq[] = new int[26];
+
+       for (int i = 0; i < str.length(); i++) {
+        char ch = str.charAt(i);
+        q.add(ch);
+        freq[ch-'a']++;
+        while (!q.isEmpty() && freq[q.peek() - 'a'] > 1) {
+            q.remove();
+        }
+        if (q.isEmpty()) {
+            System.out.print(-1 +" ");
+        }else {
+            System.out.print(q.peek()+" ");
+        }
+       }
+
+    }
+
+    public static void main(String[] args) {
+        String str = "aabccxb";
+        printFirstNonRepeating(str);
+    }
+}
