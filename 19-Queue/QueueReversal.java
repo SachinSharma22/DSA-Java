4106b15bcf802d1662e99c2b3c43413c23b112b5
SachinSharma22 <s830sharma@gmail.com>
2026-03-28 10:04:26 +0530

    Reverse a queue with the help of stack

---
+++ 19-Queue/QueueReversal.java
@@ -0,0 +1,30 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class QueueReversal {
+    public static void reverseQueue(Queue<Integer> q){
+        Stack<Integer> st = new Stack<>();
+       while(!q.isEmpty()) {
+            st.push(q.remove());
+        }
+        while (!st.isEmpty()) {
+            q.add(st.pop());
+        }
+    }
+
+    public static void main(String[] args) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        q.add(5);
+
+        reverseQueue(q);
+
+        while (!q.isEmpty()) {
+            System.out.print(q.remove() + " ");
+        }
+    }
+}
