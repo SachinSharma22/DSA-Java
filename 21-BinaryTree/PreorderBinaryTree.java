517ea5e57b6a0ceee423202cd36098fedeb12de6
SachinSharma22 <s830sharma@gmail.com>
2026-03-31 17:47:54 +0530

    Inroder, PreOrder, and PostOrder traversal in a binary tree

--- 21-BinaryTree/PreorderBinaryTree.java
+++ 21-BinaryTree/PreorderBinaryTree.java
@@ -23,13 +23,57 @@ public class PreorderBinaryTree {
             newNode.right = buildTree(nodes);
             return newNode;
         }
+
+        // Preorder traversal
+        public void preorder(Node root) {
+            if(root == null ) {
+                return;
+            }
+            System.out.print(root.data + " ");
+            preorder(root.left);
+            preorder(root.right);
+
+        }
+
+        //inorder traversal
+        public void inorder(Node root) {
+            if(root == null) {
+                return;
+            }
+            inorder(root.left);
+            System.out.print(root.data + " ");
+            inorder(root.right);
+        }
+
+        //post order traversal
+
+        public void postOrder(Node root) {
+            if(root == null) {
+                return;
+            }
+            postOrder(root.left);
+            postOrder(root.right);
+            System.out.print(root.data + " ");
+        }
     }
+
+
 
     public static void main(String[] args) {
         int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
 
         BinrayTree tree = new BinrayTree();
         Node root = tree.buildTree(nodes);
-        System.out.println(root.data);
+        // System.out.println(root.data);
+
+        // preorder print
+        // tree.preorder(root);
+
+        //Inorder traversal
+        // tree.inorder(root);
+
+        // Postorder traversal
+
+        tree.postOrder(root);
     }
 }
