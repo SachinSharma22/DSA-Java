49260bf75a5c8148ee9cf2d2652ece0f830a7461
SachinSharma22 <s830sharma@gmail.com>
2026-04-01 12:26:43 +0530

    find that the provided number is power of two or not

---
+++ 12-Recursion/PowerOfTwo.java
@@ -0,0 +1,19 @@
+public class PowerOfTwo {
+    public static boolean isPowerOfTwo(int n) {
+        if(n<=0) {
+            return false;
+        }
+        if(n == 1) {
+            return true;
+        }
+        if(n%2==0) {
+            return isPowerOfTwo(n/2);
+        }
+        return false;
+        
+    }
+    public static void main(String[] args) {
+        int n = 11;
+        System.out.println(isPowerOfTwo(n));
+    }
+}
