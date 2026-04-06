01fd4c2982c84c79ae1457819e236b30b910b72e
SachinSharma22 <s830sharma@gmail.com>
2026-04-06 13:55:02 +0530

    check that tree is a valid BST or not

--- 22-BinarySearchTree/BuildBST.java
+++ 22-BinarySearchTree/BuildBST.java
@@ -117,8 +117,25 @@ public class BuildBST {
         path.remove(path.size()-1);
     }
 
+
+    //Is Valid BST
+    public static boolean isValidBST(Node root, Node min, Node max) {
+        if(root == null) {
+            return true;
+        }
+
+        if (min!=null && root.data <= min.data) {
+            return false;
+        }else if(max!= null && root.data >= max.data) {
+            return false;
+        }
+
+        return isValidBST(root.left, min, root) && isValidBST(root.right, root, max);
+
+    }
+
     public static void main(String[] args) {
-        int values[] = {8,5,3,1,4,6,10,11,14};
+        int values[] = {8,5,3,6,10,11,14};
         Node root = null;
 
         for (int i = 0; i < values.length; i++) {
@@ -144,6 +161,13 @@ public class BuildBST {
 
         //print path to leaf Node
         
-        printRoot2Leaf(root, new ArrayList<Integer>());
+        // printRoot2Leaf(root, new ArrayList<Integer>());
+
+        //check valid BST
+        if(isValidBST(root, null, null)) {
+            System.out.println("Valid");
+        }else {
+            System.out.println("InValid");
+        }
     }
 }
