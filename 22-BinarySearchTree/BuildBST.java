2c153b0c5ebad53689aea38c4a5a63758414cd59
SachinSharma22 <s830sharma@gmail.com>
2026-04-06 11:17:50 +0530

    print all node lies between k1 and k2

--- 22-BinarySearchTree/BuildBST.java
+++ 22-BinarySearchTree/BuildBST.java
@@ -77,6 +77,23 @@ public class BuildBST {
         return root;
     }
 
+    //print in range of k1 and k2
+
+    public static void printInRange(Node root, int k1, int k2) {
+        if(root == null) {
+            return;
+        }
+        if(root.data >= k1 && root.data <= k2) {
+            printInRange(root.left, k1, k2);
+            System.out.print(root.data + " ");
+            printInRange(root.right, k1, k2);
+        }else if(root.data < k1) {
+            printInRange(root.left, k1, k2);
+        }else{
+            printInRange(root.right, k1, k2);
+        }
+    }
+
     public static void main(String[] args) {
         int values[] = {8,5,3,1,4,6,10,11,14};
         Node root = null;
@@ -88,9 +105,9 @@ public class BuildBST {
         inorder(root);
         System.out.println();
 
-        //delete node
-        delete(root, 5);
-        inorder(root);
+        // //delete node
+        // delete(root, 5);
+        // inorder(root);
 
         // //search
         // if(search(root, 11)){
@@ -98,5 +115,8 @@ public class BuildBST {
         // }else{
         //     System.out.println("NOT FOUND");
         // }
+
+        //print in range k1 and k2
+        printInRange(root, 5, 12);
     }
 }
