c578ca205bd94147db753d4608d2790ee6b0fe1d
SachinSharma22 <s830sharma@gmail.com>
2026-04-12 23:59:50 +0530

    adding optimize way for same

--- 07-Arrays/ContainsDuplicate.java
+++ 07-Arrays/ContainsDuplicate.java
@@ -1,4 +1,15 @@
+import java.util.Arrays;
+
 public class ContainsDuplicate {
+    public boolean containsDuplicate1(int[] nums) {
+        Arrays.sort(nums);
+        for(int i = 1; i < nums.length; i++) {
+            if(nums[i] == nums[i-1]) {
+                return true;
+            }
+        }
+        return false;
+    }
     public static boolean containsDuplicate(int[] nums) {
         for(int i = 0; i< nums.length; i++){
             for(int j = i+1; j<nums.length;j++){
