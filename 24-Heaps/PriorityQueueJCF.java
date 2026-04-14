5fcecefa5eee574673f4166a165bd361f896e2e4
SachinSharma22 <s830sharma@gmail.com>
2026-04-14 22:37:31 +0530

    Make priority to larger numbers

--- 24-Heaps/PriorityQueueJCF.java
+++ 24-Heaps/PriorityQueueJCF.java
@@ -1,9 +1,13 @@
+import java.util.Comparator;
 import java.util.PriorityQueue;
 
 public class PriorityQueueJCF {
 
     public static void main(String[] args) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        // PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        // This line is used to give priority to larger numbers
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
         pq.add(4);
         pq.add(3);
         pq.add(1);
