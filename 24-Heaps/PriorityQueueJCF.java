ccae6ac0d130cec1a150587a86b1ab5597f3017a
SachinSharma22 <s830sharma@gmail.com>
2026-04-15 18:18:36 +0530

    comapre and print student according to their rank

--- 24-Heaps/PriorityQueueJCF.java
+++ 24-Heaps/PriorityQueueJCF.java
@@ -1,11 +1,11 @@
-import java.util.Comparator;
+// import java.util.Comparator;
 import java.util.PriorityQueue;
 
 public class PriorityQueueJCF {
     static class Student implements Comparable<Student>{
         String name;
         int rank;
-        Student(String name, int rank) {
+       public Student(String name, int rank) {
             this.name = name;
             this.rank = rank;
         }
@@ -17,17 +17,18 @@ public class PriorityQueueJCF {
     }
 
     public static void main(String[] args) {
-        // PriorityQueue<Integer> pq = new PriorityQueue<>();
+        PriorityQueue<Student> pq = new PriorityQueue<>();
 
         // This line is used to give priority to larger numbers
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
-        pq.add(4);
-        pq.add(3);
-        pq.add(1);
-        pq.add(7);
+        // PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+        pq.add(new Student("A", 4));
+        pq.add(new Student("B", 5));
+        pq.add(new Student("C", 2));
+        pq.add(new Student("D", 12));
 
         while(!pq.isEmpty()) {
-            System.out.print(pq.peek() + " ");
+            System.out.println(pq.peek().name + " -> " + pq.peek().rank + " ");
+            
             pq.remove();
         }
     }
