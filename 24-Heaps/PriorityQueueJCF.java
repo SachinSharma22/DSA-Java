576cc3d81ca7d65e2ed86d9201f105386dd5f964
SachinSharma22 <s830sharma@gmail.com>
2026-04-15 18:07:05 +0530

    define comparable class for heaps

--- 24-Heaps/PriorityQueueJCF.java
+++ 24-Heaps/PriorityQueueJCF.java
@@ -2,6 +2,19 @@ import java.util.Comparator;
 import java.util.PriorityQueue;
 
 public class PriorityQueueJCF {
+    static class Student implements Comparable<Student>{
+        String name;
+        int rank;
+        Student(String name, int rank) {
+            this.name = name;
+            this.rank = rank;
+        }
+
+        @Override
+        public int compareTo(Student s2) {
+            return this.rank - s2.rank;
+        }
+    }
 
     public static void main(String[] args) {
         // PriorityQueue<Integer> pq = new PriorityQueue<>();
