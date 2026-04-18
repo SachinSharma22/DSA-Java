e2ef989680a55a52bc80013caa1388562d167776
SachinSharma22 <s830sharma@gmail.com>
2026-04-18 08:49:46 +0530

    basic operations on HashMap like put remove get clear size and isEmpty

--- 25-Hashing/HashingBasic.java
+++ 25-Hashing/HashingBasic.java
@@ -4,10 +4,38 @@ public class HashingBasic {
 
     public static void main(String[] args) {
         HashMap<String, Integer> hm = new HashMap<>();
+
+        // Insert O(1)
         hm.put("India", 100);
         hm.put("China", 105);
         hm.put("US", 50);
 
         System.out.println(hm);
+
+        // Get O(1)
+
+        // int popuInd = hm.get("India");
+        // System.out.println("Population of India is = " + popuInd);
+
+        // Invalid key return null
+        // System.out.println(hm.get("Thailand"));
+
+        // containsKey()
+        // System.out.println(hm.containsKey("India"));
+        // System.out.println(hm.containsKey("Thailand"));
+
+        // Remove()
+        // System.out.println(hm.remove("China"));
+        // System.out.println(hm);
+
+        //size
+        System.out.println(hm.size());
+
+        //isEmpty()
+        System.out.println(hm.isEmpty());
+
+        //clear() --> clear all the data from HashMap
+        hm.clear();
+        System.out.println(hm.isEmpty());
     }
 }
