ee8b682869808b815488b4e464abcc260622d854
SachinSharma22 <s830sharma@gmail.com>
2026-04-25 22:05:38 +0530

    Tree Set in Java

---
+++ 25-Hashing/TreeSetJava.java
@@ -0,0 +1,15 @@
+import java.util.TreeSet;
+
+public class TreeSetJava {
+
+    public static void main(String[] args) {
+        TreeSet<String> ts = new TreeSet<>();
+        ts.add("delhi");
+        ts.add("Sonbhadra");
+        ts.add("Nepal");
+        ts.add("thailand");
+        ts.add("mumbai");
+
+        System.out.println(ts);
+    }
+}
