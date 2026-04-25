e45a4dfda3d507f6c54936211d236a18edd94e03
SachinSharma22 <s830sharma@gmail.com>
2026-04-25 21:44:54 +0530

    iteration on hash set

---
+++ 25-Hashing/IteratorOnHashSet.java
@@ -0,0 +1,20 @@
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class IteratorOnHashSet {
+
+    public static void main(String[] args) {
+        HashSet<String> cities = new HashSet<>();
+        cities.add("delhi");
+        cities.add("Sonbhadra");
+        cities.add("Nepal");
+        cities.add("thailand");
+        cities.add("mumbai");
+        Iterator it = cities.iterator();
+
+        while (it.hasNext()) {
+            System.out.println(it.next());
+        }
+        
+    }
+}
