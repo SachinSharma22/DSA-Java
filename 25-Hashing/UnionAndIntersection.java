63885559434f86f52e27e864bb2cc78a026f429a
SachinSharma22 <s830sharma@gmail.com>
2026-04-28 08:14:50 +0530

    find intersection of given two array

--- 25-Hashing/UnionAndIntersection.java
+++ 25-Hashing/UnionAndIntersection.java
@@ -15,11 +15,24 @@ public class UnionAndIntersection {
     }
 
     public static void intersection(int[] num1, int[] num2) {
+        HashSet<Integer> isec = new HashSet<>();
+        for (int i = 0; i < num1.length; i++) {
+            isec.add(num1[i]);
+        }
+        int count = 0;
+        for (int i = 0; i < num2.length; i++) {
+          if(isec.contains(num2[i])){
+            count++;
+            isec.remove(num2[i]);
+          }
+        }
 
+        System.out.println("Intersection size = " + count);
     }
     public static void main(String[] args) {
        int arr1[] = {7,3,9};
        int arr2[] = {6,3,9,2,9,4};
        union(arr1, arr2);
+       intersection(arr1, arr2);
     }
 }
